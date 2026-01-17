004554cdf6c7643ba43285f130178bb38abd2642
raman <ramanj786@gmail.com>
2026-01-17 19:48:14 +0530

    some message-new

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#During run script can mention names/vales.
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zomm webonar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: YES,if you make ukraine joins Russia"
+
+

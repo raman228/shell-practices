6ab97d035e4127273998aa468f8bf28c7a9fdd56
raman <ramanj786@gmail.com>
2026-01-17 23:01:21 +0530

    some message-new

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export COURSE= "DevSecops with AWS"
+
+# Env variable, this varibale is valid untill this process is alive, its is accsssible to sub process also.
+
+## to check/verify
+
+echo $COURSE
+
+#root@vm01:~# export COURSE="DevSecops with AWS"
+#root@vm01:~# echo $COURSE
+#DevSecops with AWS
+#root@vm01:~#

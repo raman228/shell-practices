e6ef58104f2db44137c34eab58318414e64c6bea
raman <ramanj786@gmail.com>
2026-01-17 16:33:21 +0530

    some message-new

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Ramamesh: Hi Suresh, How are you ?
+Suresh : Hello Ramesh, I am fine, How are you ?
+Ramesh : what are you learnig recently ?
+Suresh: I am learning shell scripting Ramesh.

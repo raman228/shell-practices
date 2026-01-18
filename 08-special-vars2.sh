b1b9b61987b202a645034dc00a278a49309a5ed8
raman <ramanj786@gmail.com>
2026-01-18 14:32:30 +0530

    some message-new

--- 08-special-vars2.sh
+++ 08-special-vars2.sh
@@ -2,10 +2,15 @@
 
 # Below are the some special variables.
 
-echo "current directory: $PWD"
-echo "who is running this: $USER"
-echo "Home Directory of user: $HOME
-echo "PID of this script: $$"
-sleep 50 & ## this wull goes to background.
-echo "PID of the last command in background is: $!"
+#!/bin/bash
 
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+
+echo "script name: $0"
+echo "current working direcory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 50 &
+echo "PID of the last command in the back ground is: $!"

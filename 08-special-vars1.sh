db47b41feb97ff0841c27da1f5f8a47cc724fe11
raman <ramanj786@gmail.com>
2026-01-18 14:10:38 +0530

    some message-new

---
+++ 08-special-vars1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# I want my script name end of the script output.
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+
+echo "script name: $0"
+
+
